032b44ae3a505e2e3d088ded57b9e5de64d731eb
Jonathan Urquiza <juruqiza86@hotmail.com>
2025-09-25 23:46:11 -0300

    first commit

--- pharma/settings.py
+++ pharma/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&tyemyoedsf%#74x-_0)0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,0.0.0.0').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,0.0.0.0,audioslash.onrender.com,*.onrender.com').split(',')
 
 
 # Application definition
