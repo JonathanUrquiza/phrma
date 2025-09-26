a133d48eb42bb52a696aae335fb04a81a4778dc6
Jonathan Urquiza <juruqiza86@hotmail.com>
2025-09-26 00:12:34 -0300

    first commit

--- pharma/settings.py
+++ pharma/settings.py
@@ -148,6 +148,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",  # Formulario web interactivo
     ],
     "DEFAULT_PARSER_CLASSES": [
         "rest_framework.parsers.JSONParser",
